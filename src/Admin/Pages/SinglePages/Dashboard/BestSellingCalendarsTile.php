97c3848c82d79ba39184b1b20e4ea34c7f27d0a7
Tim Lappe <mail@tlappe.de>
2021-11-20 00:18:56 +0100

    Implement more Dashboard Tiles und bug fixes

---
+++ src/Admin/Pages/SinglePages/Dashboard/BestSellingCalendarsTile.php
@@ -0,0 +1,46 @@
+<?php
+
+
+namespace TLBM\Admin\Pages\SinglePages\Dashboard;
+
+
+use TLBM\Calendar\CalendarManager;
+use TLBM\Calendar\CalendarStatistics;
+use TLBM\Model\CalendarSelection;
+
+class BestSellingCalendarsTile extends DashboardTile {
+
+	public function __construct() {
+		parent::__construct( __("Top Calendars in last 30 days", TLBM_TEXT_DOMAIN) );
+	}
+
+	public function PrintBody(): void {
+		$now = new \DateTime();
+		$now->sub(new \DateInterval("P30D"));
+		$bestselling = CalendarStatistics::GetBestSellingCalendars($now);
+		arsort($bestselling);
+
+		if(sizeof($bestselling) > 0) {
+			echo "<ul class='tlbm-dashboard-tile-best-selling-list'>";
+			$c = 0;
+			foreach ( $bestselling as $id => $num ) {
+				$cal = CalendarManager::GetCalendar( $id );
+				echo "<li>";
+				echo "<a href='" . get_edit_post_link( $id ) . "'>" . $cal->title . "</a><br>";
+				echo $num . __( " Booking", TLBM_TEXT_DOMAIN );
+				echo "</li>";
+				$c++;
+
+				if($c == 5) {
+					break;
+				}
+			}
+			echo "</ul>";
+		} else {
+			?>
+			<span class="tlbm-text-big-light"><?php _e("No bookings in the last 30 days", TLBM_TEXT_DOMAIN) ?></span>
+			<?php
+		}
+
+	}
+}
---
+++ src/Admin/Pages/SinglePages/Dashboard/DatesTodayTile.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace TLBM\Admin\Pages\SinglePages\Dashboard;
+
+
+class DatesTodayTile extends DashboardTile {
+
+	public function __construct() {
+		parent::__construct( __("Dates Today", TLBM_TEXT_DOMAIN) );
+	}
+
+	public function PrintBody(): void {
+
+	}
+}
