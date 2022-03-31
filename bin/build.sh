32e7831ec1fab48aa1d50252c18a890864450723
Tim Lappe <mail@tlappe.de>
2022-03-31 14:14:13 +0200

    fix bit database type for mysql 57 Platform

---
+++ bin/build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+buildPath="booking-magic"
+srcPath=".."
+
+cd ..
+composer install
+
+cd bin
+
+rm -r $buildPath
+
+mkdir $buildPath
+mkdir $buildPath/docs
+mkdir $buildPath/lang
+mkdir $buildPath/assets
+mkdir $buildPath/src
+mkdir $buildPath/templates
+mkdir $buildPath/vendor
+
+cp -r $srcPath/docs $buildPath
+cp -r $srcPath/assets $buildPath
+cp -r $srcPath/lang $buildPath
+cp -r $srcPath/src $buildPath
+cp -r $srcPath/templates $buildPath
+cp -r $srcPath/vendor $buildPath
+cp  $srcPath/booking-magic.php $buildPath
+cp  $srcPath/config.php $buildPath
+cp  $srcPath/constants.php $buildPath
+cp  $srcPath/dependency.php $buildPath
+cp  $srcPath/README.md $buildPath
+cp  $srcPath/startup.php $buildPath
+
+zip -r booking-magic.zip $buildPath
